fbe8d6d0bcebf3022f73a5b74c68e4733c9c7438
ja-odur <odurjoseph8@gmail.com>
2025-06-10 12:19:13 +0300

    Refractor to include Conv struct

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,16 +1,16 @@
 defmodule Servy.Parser do
+  alias Servy.Conv
+
   def parse(request) do
     [method, path, _] =
       request
       |> String.split("\n")
       |> List.first()
-      |> String.split(" ") 
+      |> String.split(" ")
 
-    %{
+    %Conv{
       method: method,
-      path: path,
-      resp_body: "",
-      status: nil
+      path: path
     }
   end
 end
